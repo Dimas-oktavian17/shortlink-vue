3dd8872c4623d9d5dcf353ea0f242868c6d78ae7
Dimas-oktavian17 <dimas.ooktavian.17@gmail.com>
2023-12-05 15:41:39 +0700

    add atomic component and create some api function

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <script setup>
+import { reactive, toRefs } from 'vue'
 import HeaderComponent from './components/headerComponent.vue'
+import MainComponent from './components/mainComponent.vue'
+const Title = reactive({
+    headerTitle: 'Shortly',
+    mainTitle: 'More than just shorter links'
+})
+const { headerTitle, mainTitle } = toRefs(Title)
 </script>
 
 <template>
-    <HeaderComponent></HeaderComponent>
+    <HeaderComponent :title="headerTitle"></HeaderComponent>
+    <MainComponent :title="mainTitle" />
 </template>
 
---
+++ src/components/cardsFigure.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { reactive, toRefs } from 'vue'
+import ilustrasion from '../assets/icon-brand-recognition.svg'
+const figure = reactive({
+    title: 'icon brand',
+    link: ilustrasion
+})
+const { title, link } = toRefs(figure)
+</script>
+
+<template>
+    <figure>
+        <img :src="link" :alt="title">
+    </figure>
+</template>
